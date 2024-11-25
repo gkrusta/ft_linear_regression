0478a9bb9849bb05943ba97a9b234f94cb1f046d
gkrusta <krustagabriela@gmail.com>
2024-11-25 16:22:10 +0100

    libraries add

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,5 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 #recibe las dos listas. calcula suma al cuadrado de la diferencia de los dos
 def mean_squared_error(predictions, actual_values):
     m = len(actual_values)
-    
+
+
+def process_file():
+    data = pd.read_csv("data.csv")
